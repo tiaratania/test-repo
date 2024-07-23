9078e8f325b5ab13b7cc9b93add711fdc9299ec2
tiara <tiarataniaa@gmail.com>
2024-07-23 16:03:49 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-# Create the network if it doesn't already exist
-docker network ls | grep -q test-repo-net || docker network create test-repo-net
-
 echo 'Now...'
 docker run -d --network test-repo-net -p 84:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/test-repo/src:/var/www/html php:7.2-apache
 sleep 1
