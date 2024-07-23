3ab09ad440e135b827d346756e13083d1d2096b4
tiara <tiarataniaa@gmail.com>
2024-07-23 16:27:05 +0800

    TEST

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,7 @@ set -x
 
 echo 'Now...'
 # Ensure you change the network to jenkins-php-selenium-test-1_jenkins-net
-docker run -d --network jenkins-php-selenium-test-1_jenkins-net -p 85:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/test-repo/src:/var/www/html php:7.2-apache
+docker run -d --network jenkins-php-selenium-test-1_jenkins-net -p 85:80 --name my-apache-php-app-1 -v /Users/tiara/Documents/GitHub/test-repo/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
